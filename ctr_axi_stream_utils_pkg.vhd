4c13b75c771bdc6b15d6d8dd1323e5ed23af0a42
Pablo Leyva <pablo.leyva@huawei.com>
2020-03-09 17:28:20 +0100

    Added support for array level check of transfers

--- ctr_axi_stream_utils_pkg.vhd
+++ ctr_axi_stream_utils_pkg.vhd
@@ -19,15 +19,18 @@ package axi_stream_utils_pkg is
     pure function get_tdata_width(   i : axi_stream_interface_t ) return natural;
     pure function get_tkeep_width(   i : axi_stream_interface_t ) return natural;
     pure function get_tuser_width(   i : axi_stream_interface_t ) return natural;
-    
+
     pure function get_tdata_width(   i : axi_stream_interface_cnf_t ) return natural;
     pure function get_tkeep_width(   i : axi_stream_interface_cnf_t ) return natural;
     pure function get_tuser_width(   i : axi_stream_interface_cnf_t ) return natural;
-    
+
     pure function is_ready(          i : axi_stream_interface_t ) return boolean;
     pure function is_valid(          i : axi_stream_interface_t ) return boolean;
     pure function is_beat(           i : axi_stream_interface_t ) return boolean;
     pure function is_last_beat(      i : axi_stream_interface_t ) return boolean;
+
+    pure function all_interfaces_ready ( ia : axi_stream_ack_array_t ) return boolean;
+    pure function all_interfaces_valid ( ia : axi_stream_req_array_t ) return boolean;
 
 end axi_stream_utils_pkg;
 
@@ -39,13 +42,13 @@ package body axi_stream_utils_pkg is
     ) return axi_stream_interface_t is
         variable axi_stream_interface_t : axi_stream_interface_t( r.tdata(data_width-1   downto 0),
                                                                   r.tkeep(data_width/8-1 downto 0),
-                                                                  r.tuser(user_width-1   downto 0 )); 
-    begin 
+                                                                  r.tuser(user_width-1   downto 0 ));
+    begin
         return axi_stream_interface_t;
     end;
 
-    pure function new_axi_stream_interface( axis_cnf : axi_stream_interface_cnf_t ) return axi_stream_interface_t is 
-    begin 
+    pure function new_axi_stream_interface( axis_cnf : axi_stream_interface_cnf_t ) return axi_stream_interface_t is
+    begin
         return new_axi_stream_interface( data_width => axis_cnf.data_width,
                                          user_width => axis_cnf.user_width );
     end;
@@ -99,5 +102,23 @@ package body axi_stream_utils_pkg is
     begin
     	return is_beat(i) and get_tlast(i) = '1';
     end function is_last_beat;
+
+    pure function all_interfaces_ready( ia : axi_stream_ack_array_t ) return boolean is
+      variable all_interfaces_ready_v : boolean := false;
+    begin
+      loop_interfaces : for i in 0 to ia'length-1 loop
+          all_interfaces_ready_v := all_interfaces_ready_v or ia(i).tready='1';
+      end loop;
+      return all_interfaces_ready_v;
+    end function all_interfaces_ready;
+
+    pure function all_interfaces_valid( ia : axi_stream_req_array_t ) return boolean is
+      variable all_interfaces_valid_v : boolean := false;
+    begin
+      loop_interfaces : for i in 0 to ia'length-1 loop
+          all_interfaces_valid_v := all_interfaces_valid_v or ia(i).tvalid='1';
+      end loop;
+      return all_interfaces_valid_v;
+    end function all_interfaces_valid;
 
 end axi_stream_utils_pkg;
